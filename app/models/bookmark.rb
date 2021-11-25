1b248aae90a74d65d78dbfb9f54631c00d2358fd
vishuddha vithanage <dvishuddha@gmail.com>
2021-11-25 21:44:14 +1100

    deploy

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: {minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list_id
-  }
+  validates :movie_id, uniqueness: { scope: :list_id}
+  def self.find_by_movie_and_list(movie, list)
+    Bookmark.find_by(list: list, movie: movie)
+  end
 end
